8488e2920b8adb054fee726fde9e54efe586b0d6
DileeSandaruwan <dileepa19990801@gmail.com>
2022-02-18 16:50:53 -0500

    Get sum of given integers in the array

--- Hello_Array.cpp
+++ Hello_Array.cpp
@@ -5,3 +5,10 @@ int main() {
     int i, sum = 0;
     //declaring an array
     int numbers[10] = { 10,20,30,40,50,60,70,80,90,100 };
+
+    //to find sum of the array numbers
+    for (i = 0; i < 10; i++) {
+        sum = sum + numbers[i];
+    }
+
+    printf("Sum is %d\n", sum);
